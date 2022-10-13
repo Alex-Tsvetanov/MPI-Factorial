709de8dd3f8c49569564d785574d7bdeea9a41a2
Alex Tsvetanov <alex_tsvetanov_2002@abv.bg>
2022-10-14 00:25:48 +0300

    Initial performance test

---
+++ factorial.hpp
@@ -0,0 +1,98 @@
+#ifndef __BIG_INTEGER_FACTORIAL_HPP__
+#define __BIG_INTEGER_FACTORIAL_HPP__
+#include "./big_int.hpp" // BigUInt
+#include <thread> // std::thread
+#include <future> // std::mutex
+
+/**
+ * @brief Used to do multiplication of the threads before they are all completed.
+*/
+class _AccumulatedResult {
+private:
+
+    /**
+     * @brief Internal mutex to lock the multiplication process.
+    */
+    std::mutex processing;
+    
+    /**
+     * @brief Result of the multiplications.
+    */
+    BigUInt result;
+
+    /**
+     * @brief Number of multipliers to be multiplied.
+    */
+    size_t units;
+
+    /**
+     * @brief External mutex to be unlocked after processing.
+    */
+    std::mutex& accumulating;
+    
+    _AccumulatedResult(std::mutex& _accumulating, size_t _processes) : accumulating(_accumulating), units(_processes) {
+        result = 1;
+    }
+    
+    /**
+     * @brief Upon new multiplier incoming, multiplying the result with it.
+    */
+    void add(const BigUInt& thread_result) {
+        processing.lock();
+        result *= thread_result;
+        units --;
+        if (units == 0) accumulating.unlock();
+        processing.unlock();
+    } 
+
+public:
+    friend BigUInt factorial(unsigned int, unsigned int);
+};
+
+/**
+ * @brief Multiplies all i where i >= start, i <= N, and i%step = start
+ * @param start The starting number for the multiplication 
+ * @param step The step of increasing of the numbers for the multiplication 
+ * @param N The biggest number in the multiplication
+ * @return product of all i where i >= start, i <= N, and i%step = start
+*/
+BigUInt multiply (unsigned int start, unsigned int step, unsigned int N) {
+    BigUInt result (1);
+    for (unsigned int i = start ; i <= N ; i += step) {
+        result *= i;
+    }
+    return result;
+}
+
+/**
+ * @brief N! = factorial of N
+ * @param N The function will return factorial of this number
+ * @param processes The number of parallel processes that the function will run on 
+ * @return N! aka factorial of N
+*/
+BigUInt factorial(unsigned int N, unsigned int processes) {
+    if (processes == 1) {
+        BigUInt result (1);
+        for (unsigned long long i = 2 ; i <= N ; i ++) {
+            result *= i;
+        }
+        return result;
+    }
+    else {        
+        std::mutex accumulating;
+        accumulating.lock(); // locking the processing of the factorial
+
+        _AccumulatedResult accumulated_result(accumulating, processes);
+
+        for (unsigned int i = 1 ; i <= processes ; i ++) {
+            std::thread([&accumulated_result, i, &processes, &N] {
+                accumulated_result.add(multiply(i, processes, N));
+            }).detach(); // parallel thread started with the sub-multiplication
+        }
+
+        accumulating.lock(); // waiting for all the processing to be over
+
+        return accumulated_result.result;
+    }
+}
+#endif
---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <iostream> // std::cout
+#include <cstring> // strcmp
+#include "./factorial.hpp"
+
+/**
+ * @brief main function of Factorial MPI
+ * @param args is the number of arguments 
+ * @param argv[0] is the command name
+ * @param argv[1-4] are the prameters of the command
+ * @return exit code: 0 - success, 1 - failure
+*/
+int main(int args, char** argv) {
+
+    size_t number_of_processors, factorial_of;
+
+    for (size_t i = 1 ; i < args ; i ++) {
+        if (strcmp(argv[i], "-np") == 0) {
+            number_of_processors = atoi(argv[i + 1]);
+            i++;
+        }
+        else if (strcmp(argv[i], "-n") == 0) {
+            factorial_of = atoi(argv[i + 1]);
+            i ++;
+        }
+        else {
+            std::cout << "Unknown parameters.";
+            return 1;
+        }
+    }
+
+    std::cout << factorial(factorial_of, number_of_processors) << std::endl;  
+
+    return 0; // successful execution
+}
